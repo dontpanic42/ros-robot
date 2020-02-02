09e8e1a6170109ff87f2e3eca0c80981f4285c02
Daniel <your@example.com>
2020-02-02 07:02:31 -0500

    Set up drive for lwheel, rwheel

--- src/motor_pkg/scripts/motor.py
+++ src/motor_pkg/scripts/motor.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 import rospy
-import motor_package.listener
+import motor_package.talker
 
 if __name__ == '__main__':
 	rospy.init_node('motor')
-	motor_package.listener.listen()
+	#motor_package.listener.listen()
+	motor_package.talker.talk()
