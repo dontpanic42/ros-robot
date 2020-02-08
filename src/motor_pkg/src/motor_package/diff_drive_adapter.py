4718dbd722742e8e7297882c7cdd69d901beed19
Daniel <your@example.com>
2020-02-08 13:01:27 -0500

    Adding some comments

--- src/motor_pkg/src/motor_package/diff_drive_adapter.py
+++ src/motor_pkg/src/motor_package/diff_drive_adapter.py
@@ -15,12 +15,17 @@ class DiffDriveAdapter:
 		rospy.Subscriber("/lwheel_desired_rate", Int32, self.lwheel_desired_rate_callback)
 		rospy.Subscriber("/rwheel_desired_rate", Int32, self.rwheel_desired_rate_callback)
 
-
+	# Callback for "lwheel_desired_rate" topic message
 	def lwheel_desired_rate_callback(self, msg):
+		# Note: diff_drive publishes the rate in ticks per second. The motor controller
+		# class works with ticks per millisecond, we need to convert the values
 		rate = 0.0 if (msg.data == 0) else (float(msg.data) / 1000)
 		self.mc.set_desired_speed(lw=rate)
 
+	# Callback for "rwheel_desired_rate" topic message
 	def rwheel_desired_rate_callback(self, msg):
+		# Note: diff_drive publishes the rate in ticks per second. The motor controller
+		# class works with ticks per millisecond, we need to convert the values
 		rate = 0.0 if (msg.data == 0) else (float(msg.data) / 1000)
 		self.mc.set_desired_speed(rw=rate)
 
@@ -31,6 +36,8 @@ class DiffDriveAdapter:
 			lw_rate, rw_rate = self.mc.get_current_tickrate()
 			lw_tick, rw_tick = self.mc.get_current_ticks()
 
+			# Note: diff_drive expects the rate in ticks per second. The motor controller
+			# class works with ticks per millisecond, we need to convert the values
 			self.lwheel_rate_pub.publish(lw_rate * 1000.0)
 			self.rwheel_rate_pub.publish(rw_rate * 1000.0)
 
